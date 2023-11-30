dfe353e33c9b4ebb322880efb02aeab00c13f1ef
ShairySuarez <87496771+ShairySuarez@users.noreply.github.com>
2023-11-30 13:43:48 -0500

    Add files via upload

---
+++ js/sweetAlert.js
@@ -0,0 +1,8 @@
+Swal.fire({
+    title: "FELIZ NAVIDAD!!",
+    text: "Te Desea AndyStream",
+    imageUrl: "images/Kawai.gif",
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: "Custom image"
+  });
